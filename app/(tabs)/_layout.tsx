bb767c824a05732b1fd3c7d609154834f23a4561
Andras Petrovci <mpetrovtsiy@gmail.com>
2025-05-03 16:34:22 +0200

    Updated Tabs, added libraries for svg.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,146 +1,131 @@
 import {Tabs} from "expo-router";
-import {Image, Text, View} from "react-native";
+import {View, StyleSheet} from "react-native";
+import Home from "@/assets/home.svg"
+import SearchLogo from "@/assets/searchlogo.svg";
+import Profile from "@/assets/profile.svg";
+import Email from "@/assets/email.svg";
+import Order from "@/assets/order.svg";
+import colors from "@/styles/colors";
 
-
-function TabIcon({focused, icon, title}: any) {
-    if (focused) {
-        return (
-                <Text>
-                    {title}
-                </Text>
-        );
-    }
-
-    return (
-        <View>
-            <Image source={icon}/>
-        </View>
-    );
-}
 
 export default function TabsLayout() {
     return (
         <Tabs
             screenOptions={{
                 tabBarShowLabel: false,
-                tabBarItemStyle: {
-                    width: "100%",
-                    height: "100%",
-                    justifyContent: "center",
-                    alignItems: "center",
-                },
-                tabBarStyle: {
-                    backgroundColor: "#0F0D23",
-                    borderRadius: 50,
-                    marginHorizontal: 20,
-                    marginBottom: 36,
-                    height: 52,
-                    position: "absolute",
-                    overflow: "hidden",
-                    borderWidth: 1,
-                    borderColor: "#0F0D23",
-                },
+                headerShown: false,
+                tabBarStyle: styles.tabBar,
             }}
         >
             <Tabs.Screen
                 name="index"
                 options={{
-                    title: "index",
+                    title: "Index",
                     headerShown: false,
                     tabBarIcon: ({focused}) => (
-                        <Text>A</Text>
-                        // <TabIcon focused={focused} icon={icons.home} title="Home"/>
+                        <View style={styles.iconContainer}>
+                            <Home  height={focused ? 26 : 22}
+                                   width={focused ? 26 : 22}
+                                   color={focused ? colors.primaryGreen : colors.gray}/>
+                        </View>
+                    ),
+                }}
+            /><Tabs.Screen
+                name="messages"
+                options={{
+                    title: "Messages",
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <View style={styles.iconContainer}>
+                            <Email  height={focused ? 26 : 22}
+                                    width={focused ? 26 : 22}
+                                    color={focused ? colors.primaryGreen : colors.gray}/>
+                        </View>
+                    ),
+                }}
+            /><Tabs.Screen
+            name="search"
+            options={{
+                title: "Search",
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <View style={styles.iconContainer}>
+                        <SearchLogo   height={focused ? 46 : 42}
+                                      width={focused ? 46 : 42}
+                                     color={focused ? colors.primaryGreen : colors.gray}/>
+                    </View>
+                ),
+            }}
+        />
+            <Tabs.Screen
+            name="feed"
+            options={{
+                title: "Feed",
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <View style={styles.iconContainer}>
+                        <Order  height={focused ? 26 : 22}
+                                width={focused ? 26 : 22}
+                                color={focused ? colors.primaryGreen : colors.gray}/>
+                    </View>
+                ),
+            }}
+        />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <View style={styles.iconContainer}>
+                            <Profile  height={focused ? 26 : 22}
+                                      width={focused ? 26 : 22}
+                                      color={focused ? colors.primaryGreen : colors.gray}/>
+                        </View>
                     ),
                 }}
             />
-
-            {/*<Tabs.Screen*/}
-            {/*    name="messages"*/}
-            {/*    options={{*/}
-            {/*        title: "Messages",*/}
-            {/*        headerShown: false,*/}
-            {/*        tabBarIcon: ({focused}) => (*/}
-            {/*            <Text>D</Text>*/}
-            {/*            // <TabIcon focused={focused} icon={icons.search} title="Search"/>*/}
-            {/*        ),*/}
-            {/*    }}*/}
-            {/*/>*/}
-
-            {/*<Tabs.Screen*/}
-            {/*    name="search"*/}
-            {/*    options={{*/}
-            {/*        title: "Search",*/}
-            {/*        headerShown: false,*/}
-            {/*        tabBarIcon: ({focused}) => (*/}
-            {/*            <Text>C</Text>*/}
-            {/*            // <TabIcon focused={focused} icon={icons.save} title="Save"/>*/}
-            {/*        ),*/}
-            {/*    }}*/}
-            {/*/>*/}
-
-            {/*<Tabs.Screen*/}
-            {/*    name="feed"*/}
-            {/*    options={{*/}
-            {/*        title: "Feed",*/}
-            {/*        headerShown: false,*/}
-            {/*        tabBarIcon: ({focused}) => (*/}
-            {/*            <Text>A</Text>*/}
-            {/*            // <TabIcon focused={focused} icon={icons.person} title="Profile"/>*/}
-            {/*        ),*/}
-            {/*    }}*/}
-            {/*/>*/}
-            {/*<Tabs.Screen*/}
-            {/*    name="profile"*/}
-            {/*    options={{*/}
-            {/*        title: "Profile",*/}
-            {/*        headerShown: false,*/}
-            {/*        tabBarIcon: ({focused}) => (*/}
-            {/*            <Text>A</Text>*/}
-            {/*            // <TabIcon focused={focused} icon={icons.person} title="Profile"/>*/}
-            {/*        ),*/}
-            {/*    }}*/}
-            {/*/>*/}
         </Tabs>
     );
 }
 
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//     },
-//     tabBar: {
-//         backgroundColor: colors.white,
-//         height: 75,
-//         borderTopLeftRadius: 20,
-//         borderTopRightRadius: 20,
-//         shadowColor: '#000',
-//         shadowOffset: {width: 0, height: -3},
-//         shadowOpacity: 0.1,
-//         shadowRadius: 10,
-//         elevation: 5,
-//         borderTopWidth: 0,
-//         position: 'absolute',
-//         paddingHorizontal: 20,
-//     },
-//
-//     iconContainer: {
-//         marginTop: 5,
-//         marginBottom: -5,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-//     centerIcon: {
-//         backgroundColor: colors.primaryGreen,
-//         borderRadius: 50,
-//         width: 40,
-//         height: 40,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         shadowColor: '#000',
-//         shadowOffset: {width: 0, height: 3},
-//         shadowOpacity: 0.15,
-//         shadowRadius: 11,
-//         elevation: 5,
-//     },
-// });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    tabBar: {
+        backgroundColor: colors.white,
+        height: 75,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: -3},
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        elevation: 5,
+        borderTopWidth: 0,
+        position: 'absolute',
+        paddingHorizontal: 20,
+    },
+
+    iconContainer: {
+        marginTop: 5,
+        marginBottom: -5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    centerIcon: {
+        backgroundColor: colors.primaryGreen,
+        borderRadius: 50,
+        width: 40,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 3},
+        shadowOpacity: 0.15,
+        shadowRadius: 11,
+        elevation: 5,
+    },
+});
---
+++ styles/colors.js
@@ -0,0 +1,19 @@
+export default {
+    primaryGreen: '#0d6f57',
+    primaryBlack: '#131313',   // Другорядний колір
+    secondaryBlack: '#3D3D3D', // Первинний колір
+    background: '#FFFFFF',  // Колір фону
+    gray: 'rgb(61, 61, 61)',        // Сірий колір
+    priceGray: 'rgb(101, 101, 101)',
+    grayBackground: '#F2F2F2',
+    lightGray: '#F2F2F2',
+    error: '#FF0000',       // Колір помилки
+    darkError: '#C01818',
+    white: '#FFFFFF',       // Білий колір
+    whiteGray:'#F2F3F5',
+    warning: 'rgb(159, 184, 93)',
+    orange: 'rgb(255, 159, 67)',
+    yellow: 'rgb(255, 202, 58)',
+    verifiedColor: '#2C94FC',
+    buttonUploadGray:'#707070'
+};
